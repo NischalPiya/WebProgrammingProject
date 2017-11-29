0a140549c8fdd8de2da15a95d26300bae05f3713
Nischal <piyanischal@gmail.com>
2017-11-28 20:51:20 -0500

    Canvas_reminder

--- Server.py
+++ Server.py
@@ -1,15 +1,16 @@
 from flask import Flask, jsonify
 import requests
 import os
+import json
 
 app= Flask(__name__)
 
 import time
-start_time=time.time()
+
 canvas_api=os.environ['CANVAS_KEY']
-while True:
-    @app.route('/')
-    def index():
+
+@app.route('/')
+def index():
 
         base_url='https://canvas.moravian.edu/api/v1/users/self/courses/5519/assignments'
         headers = {"Authorization": "Bearer " + canvas_api}
@@ -19,11 +20,28 @@ while True:
         return jsonify(data)
 
 
-    if __name__=='__main__':
+if __name__=='__main__':
 
-        app.run(debug=True)
+    app.run(debug=True)
 
-    time.sleep(60.0 - ((time.time() - start_time) % 60.0))
+
+
+
+def list_assignment(course_code):
+
+        base_url = 'https://canvas.moravian.edu/api/v1/users/self/courses/'+course_code+'/assignments'
+        headers = {"Authorization": "Bearer " + canvas_api}
+        response = requests.get(base_url, headers=headers)
+        response.raise_for_status()
+        data= json.loads(response.text)
+
+        for homework in data :
+           if homework['due_at']!='None':
+            print(homework['name'],homework['assignment_group_id'],homework['due_at'])
+(list_assignment('5522'))
+
+
+
 
 
 
