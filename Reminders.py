8d5f92710aa59543cdba0b61d5f2dfa8787c1919
John Polich <stjnp20@moravian.edu>
2017-12-06 18:06:11 -0500

    list_assignment tests

--- Reminders.py
+++ Reminders.py
@@ -4,18 +4,22 @@ from Server import get_due_date,get_assign_name,list_assignment
 
 class Reminders():
 
-    def __init__(self,product_db):
+    def __init__(self,existing_reminders):
         self.r = redis.Redis()
 
     def new_canvas_reminder(self,course_code,assign_id):
-
-        if self.r.exists(get_assign_name(course_code,assign_id)) == 0:
-            self.r.set(get_assign_name(course_code,assign_id), get_due_date(course_code,assign_id))
-            return 1
+        if len(course_code) == 4 & course_code.isdigit():
+            if len(assign_id) == 5 & assign_id.isdigit():
+                if self.r.exists(get_assign_name(course_code,assign_id)) == 0:
+                    self.r.set(get_assign_name(course_code,assign_id), get_due_date(course_code,assign_id))
+                    return 1
+                else:
+                    print("This assignment already has a reminder.")
+                    return 0
+            else:
+                print("Enter a valid assignment id")
         else:
-            print("This assignment already has a reminder.")
-            return 0
-
+            print("Enter a valid course code.")
 
     def delete_canvas_reminder(self,course_code,assign_id):
         self.r.delete(get_assign_name(course_code,assign_id))
