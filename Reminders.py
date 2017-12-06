0de45352090431a7b657ad0009a3800aadd8f187
John Polich <stjnp20@moravian.edu>
2017-12-05 20:14:49 -0500

    list_assignment tests

--- Reminders.py
+++ Reminders.py
@@ -8,19 +8,11 @@ class Reminders():
         self.r = redis.Redis()
 
     def new_canvas_reminder(self,course_code,assign_id):
-        if list_assignment(course_code) == None:
-            print("Please enter a valid 4-digit assignment code")
-        elif get_assign_name(course_code,assign_id)== None:
-            print("Assignment id entered is not a class you are authorized to access")
-        elif self.r.get(course_code)== None:
-            print("Please enter a valid 4-digit class code")
-        elif self.r.get(course_code)== None:
-            print("Status code entered is not a class you are authorized to access")
+
+        if self.r.get(get_assign_name(course_code,assign_id)) != self.r.keys():
+            self.r.set(get_assign_name(course_code,assign_id), get_due_date(course_code,assign_id))
         else:
-            if self.r.get(get_assign_name(course_code,assign_id)) != self.r.keys():
-                self.r.set(get_assign_name(course_code,assign_id), get_due_date(course_code,assign_id))
-            else:
-                print("This assignment already has a reminder.")
+            print("This assignment already has a reminder.")
 
     def new_user_reminder(self,assignment_name,class_name,due_date):
         self.r.set(assignment_name,due_date)
